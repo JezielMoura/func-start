e84580f4efa8db34ec9e6c93428f1b1a486be0a3
Jeziel Moura <jezielmoura@outlook.com>
2022-09-05 07:46:01 -0300

    Fix

--- Functions/Auth.cs
+++ Functions/Auth.cs
@@ -15,7 +15,7 @@ namespace func_start
         }
 
         [Function("Auth")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
